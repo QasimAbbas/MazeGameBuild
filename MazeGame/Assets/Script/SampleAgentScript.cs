f537d95bfea6bc443a1e88eaa405839ddebea288
Qasim Abbas <qasimabbas@nbp-195-220.nbp.ruw.rutgers.edu>
2016-02-09 04:23:33 -0500

    Initial Commit

---
+++ MazeGame/Assets/Script/SampleAgentScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgentScript : MonoBehaviour {
+	NavMeshAgent agent;
+	private Vector3 target;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			RaycastHit hit;
+				
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100)) {
+
+				agent.destination = hit.point;
+			}
+
+		}
+
+
+
+		//agent.SetDestination (target.position);
+			
+			
+	}
+}
